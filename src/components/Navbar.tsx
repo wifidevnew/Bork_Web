417355f1782693a628c5821865b1fe9550405b16
wifidevnew <internetdeveloper@protonmail.com>
2024-04-10 16:45:06 +0800

    update

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,7 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import logo from "../assets/logo.svg";
-import buy from '../assets/Buy.svg'
+import buy from "../assets/Buy.svg";
 function NavbarGlobal() {
   return (
     <>
@@ -10,34 +10,47 @@ function NavbarGlobal() {
         bg="background-image: url(./public/SpaceBackground.svg);background-repeat: no-repeat; background-size: cover;"
         className="justify-between w-full flex"
       >
-        <Container>
-          <Navbar.Brand href="#home">
-            <img
-              src={logo}
-              alt="Logo"
-              width="150"
-              height="150"
-              className="d-inline-block align-top"
-            />
-          </Navbar.Brand>
-          <Nav className="flex items-center gap-16">
-            <Nav.Link className="text-white hover:scale-110 transition-transform duration-300" href="#home">
-              Twitter
-            </Nav.Link>
-            <Nav.Link className="text-white hover:scale-110 transition-transform duration-300" href="#features">
-              Telegram
-            </Nav.Link>
-            <Nav.Link className="text-white hover:scale-110 transition-transform duration-300" href="#pricing">
-            <img
-              src={buy}
-              alt="Logo"
-              width="100"
-              height="100"
-              className="d-inline-block align-top"
-            />
-            </Nav.Link>
-          </Nav>
-        </Container>
+        <div className="flex w-[100%] items-center justify-between">
+          <div className="flex ms-[15rem]">
+            <Navbar.Brand href="#home">
+              <img
+                src={logo}
+                alt="Logo"
+                width="150"
+                height="150"
+                className=" items-center"
+              />
+            </Navbar.Brand>
+          </div>
+          <div>
+            <Nav className="flex items-center gap-16 text-lg me-[10rem]">
+              <Nav.Link
+                className="text-white hover:scale-110 transition-transform duration-300"
+                href="#home"
+              >
+                Twitter
+              </Nav.Link>
+              <Nav.Link
+                className="text-white hover:scale-110 transition-transform duration-300"
+                href="#features"
+              >
+                Telegram
+              </Nav.Link>
+              <Nav.Link
+                className="text-white hover:scale-110 transition-transform duration-300"
+                href="#pricing"
+              >
+                <img
+                  src={buy}
+                  alt="Logo"
+                  width="90"
+                  height="90"
+                  className="d-inline-block align-center"
+                />
+              </Nav.Link>
+            </Nav>
+          </div>
+        </div>
       </Navbar>
     </>
   );
